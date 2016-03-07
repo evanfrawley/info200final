4c2711c1fe151c894a44d0467eecf38c3b6615ea
AbhijitPranam <abhijitpranam23@gmail.com>
2016-03-06 18:40:26 -0800

    Javascript for webpage

--- js/main.js
+++ js/main.js
@@ -1,56 +1,113 @@
-/**
- * Created by Evan on 2/27/2016.
- */
-(function() {
-    "use strict";
+jQuery(function($) {'use strict';
 
-    window.onload = function () {
-        injectEmployers();
-        //initCoursera();
-    };
+	//Responsive Nav
+	$('li.dropdown').find('.fa-angle-down').each(function(){
+		$(this).on('click', function(){
+			if( $(window).width() < 768 ) {
+				$(this).parent().next().slideToggle();
+			}
+			return false;
+		});
+	});
 
-    /*
-     *   generic ajax caller method
-     */
+	//Fit Vids
+	if( $('#video-container').length ) {
+		$("#video-container").fitVids();
+	}
 
-    function ajax(url, func) {
-        var req = new XMLHttpRequest();
-        req.onload = func;
-        req.open("GET", url, true);
-        req.send();
-    }
+	//Initiat WOW JS
+	new WOW().init();
 
+	// portfolio filter
+	$(window).load(function(){
 
-    /*
-     *      FIREBASE JS
-     */
-    //var Firebase = require("firebase");
-    var myFirebaseRef = new Firebase("https://luminous-fire-9933.firebaseio.com");
+		$('.main-slider').addClass('animate-in');
+		$('.preloader').remove();
+		//End Preloader
 
-    myFirebaseRef.child("locations/latitude").set("chinchin");
+		if( $('.masonery_area').length ) {
+			$('.masonery_area').masonry();//Masonry
+		}
 
-    /*
-     *     inject the employers
-     *
-     */
+		var $portfolio_selectors = $('.portfolio-filter >li>a');
+		
+		if($portfolio_selectors.length) {
+			
+			var $portfolio = $('.portfolio-items');
+			$portfolio.isotope({
+				itemSelector : '.portfolio-item',
+				layoutMode : 'fitRows'
+			});
+			
+			$portfolio_selectors.on('click', function(){
+				$portfolio_selectors.removeClass('active');
+				$(this).addClass('active');
+				var selector = $(this).attr('data-filter');
+				$portfolio.isotope({ filter: selector });
+				return false;
+			});
+		}
 
-    function injectEmployers() {
-        console.log("test");
-        myFirebaseRef.on("value", function (snapshot) {
-
-            var div = document.getElementById("employers");
-            snapshot = snapshot.child("Employer");
-            snapshot.forEach(function(childSnapshot){
-                var listing = document.createElement("div");
-                listing.classList.add("listing");
-                listing.innerHTML = childSnapshot.val();
-
-                div.appendChild(listing);
-            });
-
-
-        });
-    }
+	});
 
 
-})();
+	$('.timer').each(count);
+	function count(options) {
+		var $this = $(this);
+		options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+		$this.countTo(options);
+	}
+		
+	// Search
+	$('.fa-search').on('click', function() {
+		$('.field-toggle').fadeToggle(200);
+	});
+
+	// Contact form
+	var form = $('#main-contact-form');
+	form.submit(function(event){
+		event.preventDefault();
+		var form_status = $('<div class="form_status"></div>');
+		$.ajax({
+			url: $(this).attr('action'),
+			beforeSend: function(){
+				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+			}
+		}).done(function(data){
+			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+		});
+	});
+
+	// Progress Bar
+	$.each($('div.progress-bar'),function(){
+		$(this).css('width', $(this).attr('data-transition')+'%');
+	});
+
+	if( $('#gmap').length ) {
+		var map;
+
+		map = new GMaps({
+			el: '#gmap',
+			lat: 43.04446,
+			lng: -76.130791,
+			scrollwheel:false,
+			zoom: 16,
+			zoomControl : false,
+			panControl : false,
+			streetViewControl : false,
+			mapTypeControl: false,
+			overviewMapControl: false,
+			clickable: false
+		});
+
+		map.addMarker({
+			lat: 43.04446,
+			lng: -76.130791,
+			animation: google.maps.Animation.DROP,
+			verticalAlign: 'bottom',
+			horizontalAlign: 'center',
+			backgroundColor: '#3e8bff',
+		});
+	}
+
+});
